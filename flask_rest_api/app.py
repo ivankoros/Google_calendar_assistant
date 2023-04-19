6ce3fea502b05483223eb5120dcd9516cee5d144
koros <ivan.koros2@gmail.com>
2023-04-19 16:43:29 -0400

    set up baseplate REST API that returns all current events for the day as json

---
+++ flask_rest_api/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, Response, request
+from flask_restful import Resource, Api
+from google_calendar_api import get_today_events
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@app.route('/api/v1/gcal_assistant', methods=['GET'])
+def show_today_events():
+    return Response(get_today_events(), mimetype='text/plain')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
