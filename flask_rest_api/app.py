b9e6973148a9406d8d383392e930b146a8e8ead7
koros <ivan.koros2@gmail.com>
2023-04-19 16:53:13 -0400

    returned result with Response() with a mimetype of pdf/application for better compatability in the future

--- flask_rest_api/app.py
+++ flask_rest_api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
 from flask_restful import Resource, Api
 from google_calendar_api import get_today_events
 
@@ -8,7 +8,7 @@ api = Api(app)
 
 @app.route('/api/v1/gcal_assistant', methods=['GET'])
 def show_today_events():
-    return Response(get_today_events(), mimetype='text/plain')
+    return Response(get_today_events(), mimetype='application/json')
 
 
 if __name__ == '__main__':
