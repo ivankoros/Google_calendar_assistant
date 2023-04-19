8e17edad3df1c58f1e42275cb7be457a6bc724f6
koros <ivan.koros2@gmail.com>
2023-04-19 16:21:29 -0400

    created main functions initializing the Google credentials to the user and getting all events the user has today, sorted by their timezone and outputted as a nicely readable format (8:30 PM - 9:30 PM)

---
+++ main.py
@@ -0,0 +1,87 @@
+from __future__ import print_function
+
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+    Returns:
+        Credentials, the obtained credential.
+    """
+
+    scopes = ['https://www.googleapis.com/auth/calendar.readonly']
+
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', scopes)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    return creds
+
+
+def get_today_events():
+
+    creds = get_credentials()
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        calendar = service.calendars().get(calendarId='primary').execute()
+        time_zone = calendar.get('timeZone')
+
+        # Use the time zone to get today's events
+        today = datetime.datetime.now(datetime.timezone.utc).astimezone()
+        start = today.replace(hour=0, minute=0, second=0, microsecond=0).isoformat()
+        tomorrow = today + datetime.timedelta(days=1)
+        end = tomorrow.replace(hour=0, minute=0, second=0, microsecond=0).isoformat()
+
+        print('Getting today\'s events')
+        events_results = service.events().list(calendarId='primary', timeMin=start, timeMax=end, singleEvents=True, orderBy='startTime', timeZone=time_zone).execute()
+
+        events = events_results.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+            return
+
+        # Prints the start and name of the next 10 events
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            start_time_obj = datetime.datetime.strptime(start, '%Y-%m-%dT%H:%M:%S%z')
+            local_time = start_time_obj.astimezone()
+            start_time = local_time.strftime('%I:%M %p')
+
+            end = event['end'].get('dateTime', event['end'].get('date'))
+            end_time_obj = datetime.datetime.strptime(end, '%Y-%m-%dT%H:%M:%S%z')
+            local_time = end_time_obj.astimezone()
+            end_time = local_time.strftime('%I:%M %p')
+
+            print(f"{start_time} - {end_time} {event['summary']}")
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+if __name__ == '__main__':
+    get_today_events()
